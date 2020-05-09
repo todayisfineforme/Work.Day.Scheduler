6c976b5684942ffc9a032cabb5a901ec8add554e
Nathan Hall <Nashing1@gmail.com>
2020-05-09 04:53:46 -0500

    full working build
    class change on textarea dependent on moment integration, individual entries are stored to local storage and retrieved properly when reloading the page

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// code to write the rows to the DOM
-// rows need to cover one hour each 
-// each row needs an input and a button to save the input and display it
-// each row needs to save to local storage
-// needs to reference local storage 
-// rows change state based on if it is in the past, present or future
-    //grey for the past 
-    //white for present
-    //beige for the future
-
 $(document).ready(function(){
 
     var now = moment();
@@ -16,42 +6,71 @@ $(document).ready(function(){
     
     var date = null;
 
-    var schedule = {
-        nine: "",
-        ten: "",
-        eleven: "",
-        twelve: "",
-        one: "",
-        two: "",
-        three: "",
-        four: "",
-        five: ""
-    }
-
-    function update () {
-        date = moment(new Date())
-        datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-    };
+    var schedule = JSON.parse(localStorage.getItem("schedule"));
 
     $(document).ready(function(){
-        datetime = $('#currentDay')
+        datetime = $('#currentDay');
+        checkForSchedule();
+        insertSchedule();
         update();
         setInterval(update, 1000);
     });
 
-    $('textarea').each(function( index, element ){
-        console.log( $( this ));
+    function checkForSchedule(){
+        if ( schedule == null) {
+            schedule = {
+                nineAM: "",
+                tenAM: "",
+                elevenAM: "",
+                twelvePM: "",
+                onePM: "",
+                twoPM: "",
+                threePM: "",
+                fourPM: "",
+                fivePM: ""
+            };
+            localStorage.setItem("schedule", JSON.stringify(schedule));
+        } else {
+            schedule = JSON.parse(localStorage.getItem("schedule"));
+        }
+    }
+
+    function insertSchedule(){
+        $('textarea').each(function(){
+            var textBox = $(this);
+            var boxId = textBox[0].id ;
+            $(this).html(schedule[boxId]);
+        })
+    };
+
+    function update(){
+        displayTime();
+        blockState();
+    }
+
+    function displayTime () {
+        date = moment(new Date())
+        datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+    };
+
+    function blockState(){
+        $('textarea').each(function( index,element ){
+            var dataHour = (parseInt(this.dataset.hour));
+            if (now.hour() === dataHour){
+                $(this).removeClass("past present future").addClass("present");
+            }else if (now.hour() > dataHour){
+                $(this).removeClass("past present future").addClass("past");
+            }else{
+                $(this).removeClass("past present future").addClass("future");
+            };
+        });
+    }
+
+    $('.saveBtn').on('click', function(){
+        var textObject = $(this).parent().find("textarea");
+        var textValue = textObject[0].value;
+        var idValue = textObject[0].id;
+        schedule[idValue] = textValue;
+        localStorage.setItem("schedule", JSON.stringify(schedule)); 
     });
-
-    console.log(now.format("H"));
-
-
-
-
-
-
-
-
-
-
 });
